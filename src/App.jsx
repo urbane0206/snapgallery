1846d1ae31f357ed1d86f847264502831635148a
maxoumax <maxime.tonnelier01@gmail.com>
2024-04-23 03:56:58 +1100

    ajout fonction bouton sidebar hide small bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navbar } from './Components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Image from './Pages/Image/Image'
 
 const App = () => {
+
+  const [sidebar,setSidebar] = useState(true);
+
   return (
     <div>
-      <Navbar/>
+      <Navbar setSidebar={setSidebar}/>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home sidebar={sidebar} />} />
         <Route path='/image/:categoryId/:imageId' element={<Image/>} />
       </Routes>
     </div>
