faf436af877fb75a2d911efd22254a49987d24ac
maxoumax <maxime.tonnelier01@gmail.com>
2024-05-01 00:19:56 +1100

    ajout Image page

--- src/Pages/Image/Image.jsx
+++ src/Pages/Image/Image.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Image.css';
+import PlayVideo from '../../Components/PlayVideo/PlayVideo';
+import Recommanded from '../../Components/Recommended/Recommended';
+
 
 const Image = () => {
   return (
-    <div>Image</div>
+    <div className='play-container'>
+        <PlayVideo/>
+        <Recommanded/>
+    </div>
   );
 }
 
