101579f7dabcfadbe998a5a0e17c6e3fdb54dcf3
Snjsddngmnmsdfgdsfg <sean.rogers@etudiant.unc.nc>
2024-05-17 11:51:49 +1100

    Génération d'image dans le Home.jsx
    récupère les images pour les afficher dans le Home

--- Image_NodeJs/gestion_Image.js
+++ Image_NodeJs/gestion_Image.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 2001;
+const IMAGES_FOLDER = path.join(__dirname, 'Images');
 
 app.use(cors());
 
 // Configuration de Multer pour gérer les fichiers uploadés
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'Images/') // Le dossier où seront stockés les fichiers
+    cb(null, IMAGES_FOLDER) // Le dossier où seront stockés les fichiers
   },
   filename: function (req, file, cb) {
     // Génération d'un nom de fichier unique
@@ -38,7 +44,73 @@ app.post('/upload', upload.single('file'), (req, res) => {
   });
 });
 
+// Route GET pour récupérer la liste des images
+app.get('/api/images', (req, res) => {
+  fs.readdir(IMAGES_FOLDER, (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to read images directory' });
+    }
+    const images = files.filter(file => file.endsWith('.png') || file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.webp'));
+    res.json(images);
+  });
+});
+
+// Route GET pour servir les fichiers image
+app.get('/images/:filename', (req, res) => {
+  const filePath = path.join(IMAGES_FOLDER, req.params.filename);
+  res.sendFile(filePath);
+});
+
 // Démarrage du serveur
 app.listen(port, () => {
   console.log(`Serveur lancé sur http://localhost:${port}`);
 });
+
+
+
+
+// Ancien code
+// import express from 'express';
+// import multer from 'multer';
+// import path from 'path';
+// import cors from 'cors';
+//
+// const app = express();
+// const port = 2001;
+//
+// app.use(cors());
+//
+// // Configuration de Multer pour gérer les fichiers uploadés
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'Images/') // Le dossier où seront stockés les fichiers
+//   },
+//   filename: function (req, file, cb) {
+//     // Génération d'un nom de fichier unique
+//     cb(null, Date.now() + path.extname(file.originalname))
+//   }
+// });
+//
+// const upload = multer({ storage: storage });
+//
+// // Middleware pour servir les fichiers statiques dans le dossier "uploads"
+// app.use(express.static('uploads'));
+//
+// // Route POST pour gérer l'upload du fichier
+// app.post('/upload', upload.single('file'), (req, res) => {
+//   // Récupération du titre et de la description envoyés dans le formulaire
+//   const title = req.body.titre;
+//   const description = req.body.description;
+//
+//   // Renvoi d'une réponse JSON avec les informations sur l'image
+//   res.json({
+//     fileName: req.file.filename,
+//     title: title,
+//     description: description
+//   });
+// });
+//
+// // Démarrage du serveur
+// app.listen(port, () => {
+//   console.log(`Serveur lancé sur http://localhost:${port}`);
+// });
--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,117 +1,153 @@
-import React from 'react'
-import './Feed.css'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import './Feed.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Feed = () => {
-  return (
-    <div className="feed">    
-        <Link to={`Image/20/4521`} className='card'>
-            <img src={thumbnail1} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </Link>
-        <div className='card'>
-            <img src={thumbnail2} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail3} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail4} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail5} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail6} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail7} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail8} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail1} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail2} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail3} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail4} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail5} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail6} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail7} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-        <div className='card'>
-            <img src={thumbnail8} alt="" />
-            <h2>Description Photo</h2>
-            <h3>Picture Owner</h3>
-            <p>15k views &bull; 2days ago </p>
-        </div>
-    </div>
+    const [images, setImages] = useState([]);
 
-  )
+    useEffect(() => {
+        axios.get('http://localhost:2001/api/images')
+            .then(response => {
+                setImages(response.data);
+            })
+            .catch(error => {
+                console.error("There was an error fetching the images!", error);
+            });
+    }, []);
+
+    return (
+        <div className="feed">
+            {images.map((image, index) => (
+                <Link to={`Image/20/4521`} className='card' key={index}>
+                    <img src={`http://localhost:2001/images/${image}`} alt="" />
+                    <h2>Description Photo</h2>
+                    <h3>Picture Owner</h3>
+                    <p>15k views &bull; 2 days ago </p>
+                </Link>
+            ))}
+        </div>
+    );
 }
 
-export default Feed
+export default Feed;
+
+
+// ancien code
+// import React from 'react'
+// import './Feed.css'
+// import thumbnail1 from '../../assets/thumbnail1.png'
+// import thumbnail2 from '../../assets/thumbnail2.png'
+// import thumbnail3 from '../../assets/thumbnail3.png'
+// import thumbnail4 from '../../assets/thumbnail4.png'
+// import thumbnail5 from '../../assets/thumbnail5.png'
+// import thumbnail6 from '../../assets/thumbnail6.png'
+// import thumbnail7 from '../../assets/thumbnail7.png'
+// import thumbnail8 from '../../assets/thumbnail8.png'
+// import { Link } from 'react-router-dom'
+//
+// const Feed = () => {
+//   return (
+//     <div className="feed">
+//         <Link to={`Image/20/4521`} className='card'>
+//             <img src={thumbnail1} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </Link>
+//         <div className='card'>
+//             <img src={thumbnail2} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail3} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail4} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail5} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail6} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail7} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail8} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail1} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail2} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail3} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail4} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail5} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail6} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail7} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//         <div className='card'>
+//             <img src={thumbnail8} alt="" />
+//             <h2>Description Photo</h2>
+//             <h3>Picture Owner</h3>
+//             <p>15k views &bull; 2days ago </p>
+//         </div>
+//     </div>
+//
+//   )
+// }
+//
+// export default Feed
