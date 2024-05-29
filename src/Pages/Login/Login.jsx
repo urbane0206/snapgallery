a88442c7db459dc8734caab87935f2bf214bdbc2
maxoumax <maxime.tonnelier01@gmail.com>
2024-05-30 02:58:06 +1100

    modif css et loader

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Login.css';
+import './Login_Loader.css';
 import background from '../../assets/login-bg.png';
 import GitHub_logo from '../../assets/Github_logo.png';
 import axios from 'axios';
@@ -17,10 +18,28 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const savedLoadingState = sessionStorage.getItem('loading');
+    if (savedLoadingState === 'true') {
+      setLoading(true);
+    }
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    if (code) {
+      console.log("Code d'authentification:", code);
+      recupToken(code);
+    }
+  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
+    sessionStorage.setItem('loading', 'true');
     const data = { email, password };
+   
     try {
       const response = await fetch('http://localhost:2000/login', {
         method: 'POST',
@@ -29,19 +48,29 @@ const Login = () => {
       });
       if (response.status === 401) {
         setErrorMessage("Identifiants incorrects. Veuillez réessayer.");
+        setLoading(false);
+        sessionStorage.setItem('loading', 'false');
       } else if (response.status === 200) {
         const responseData = await response.json();
         setUser(responseData);
         localStorage.setItem('user', JSON.stringify(responseData));
-        setTimeout(() => navigate('/'), 2000); 
+        setTimeout(() => { 
+          navigate('/'); 
+          setLoading(false); 
+          sessionStorage.setItem('loading', 'false');
+        }, 2000); 
       }
     } catch (error) {
       console.error('Erreur lors de la soumission du formulaire:', error);
+      setLoading(false);
+      sessionStorage.setItem('loading', 'false');
     }
   };
 
   const loginWithGithub = (event) => {
     event.preventDefault();
+    setLoading(true);
+    sessionStorage.setItem('loading', 'true');
     window.location.assign(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`);
   };
 
@@ -52,12 +81,20 @@ const Login = () => {
       if (response.status === 200) {
         setUser(response.data);
         localStorage.setItem('user', JSON.stringify(response.data));
-        setTimeout(() => navigate('/'), 2000); 
+        setTimeout(() => { 
+          navigate('/'); 
+          setLoading(false); 
+          sessionStorage.setItem('loading', 'false');
+        }, 2000);  
       } else {
         console.error("Erreur:", response.status);
+        setLoading(false);
+        sessionStorage.setItem('loading', 'false');
       }
     } catch (error) {
       console.error("Erreur réseau:", error);
+      setLoading(false);
+      sessionStorage.setItem('loading', 'false');
     }
   };
 
@@ -99,73 +136,66 @@ const Login = () => {
     }
   };
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get('code');
-    if (code) {
-      console.log("Code d'authentification:", code);
-      recupToken(code);
-    }
-  }, []);
-
   return (
     <div className="login" style={{ backgroundImage: `url(${background})` }}>
-      <form onSubmit={handleSubmit} className="login__form">
-        <h1 className="login__title">Login</h1>
-
-        <div className="login__inputs">
-          <div className="login__box">
-            <input
-              type="email"
-              placeholder="Email ID"
-              required
-              className="login__input"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <i className="ri-mail-fill"></i>
-          </div>
-
-          <div className="login__box">
-            <input
-              type="password"
-              placeholder="Password"
-              required
-              className="login__input"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <i className="ri-lock-2-fill"></i>
-          </div>
-          <br />
+      {loading ? (
+        <div className="loading-container">
+          <div className="spinner"></div>
         </div>
-        {errorMessage && <div className="error-message">{errorMessage}</div>}
-        <div className="login__check">
-          <div className="login__check-box">
-            <input
-              type="checkbox"
-              className="login__check-input"
-              id="user-check"
-              checked={rememberMe}
-              onChange={(e) => setRememberMe(e.target.checked)}
-            />
-            <label htmlFor="user-check" className="login__check-label">Remember me</label>
+      ) : (
+        <form onSubmit={handleSubmit} className="login__form">
+          <h1 className="login__title">Login</h1>
+          <div className="login__inputs">
+            <div className="login__box">
+              <input
+                type="email"
+                placeholder="Email ID"
+                required
+                className="login__input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <i className="ri-mail-fill"></i>
+            </div>
+            <div className="login__box">
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                className="login__input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <i className="ri-lock-2-fill"></i>
+            </div>
+            <br />
           </div>
-          <a href="#" className="login__forgot">Forgot Password?</a>
-        </div>
-        <button type="submit" className="login__button">Login</button>
-        <button className="login__button" onClick={loginWithGithub}>
-          <div className='login_logo_container'>
-            <img className="login__logo" src={GitHub_logo} alt="" />
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+          <div className="login__check">
+            <div className="login__check-box">
+              <input
+                type="checkbox"
+                className="login__check-input"
+                id="user-check"
+                checked={rememberMe}
+                onChange={(e) => setRememberMe(e.target.checked)}
+              />
+              <label htmlFor="user-check" className="login__check-label">Remember me</label>
+            </div>
+            <a href="#" className="login__forgot">Forgot Password?</a>
           </div>
-          <span>
-            Continue with GitHub
-          </span>
-        </button>
-        <div className="login__register">
-          Don&apos;t have an account? <Link to="/Inscription">Register</Link>
-        </div>
-      </form>
+          <button type="submit" className="login__button">Login</button>
+          <button className="login__button" onClick={loginWithGithub}>
+            <div className='login_logo_container'>
+              <img className="login__logo" src={GitHub_logo} alt="GitHub Logo" />
+            </div>
+            <span>Continue with GitHub</span>
+          </button>
+          <div className="login__register">
+            Don't have an account? <Link to="/Inscription">Register</Link>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
